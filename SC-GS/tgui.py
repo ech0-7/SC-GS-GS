17bb95ef7db6c52db21616e5d37ccf1accc1447a
echo <1440204228@qq.com>
2024-03-19 21:53:52 +0800

    compare

---
+++ SC-GS/tgui.py
@@ -0,0 +1,25 @@
+import turtle
+
+pen = turtle.Turtle()
+turtle.speed(10)
+
+
+def curve():
+    for _ in range(200):
+        pen.right(1)
+        pen.forward(1)
+
+
+def heart():
+    pen.fillcolor("red")
+    pen.begin_fill()
+    pen.left(140)
+    pen.forward(113)
+    curve()
+    pen.left(120)
+    curve()
+    pen.forward(112)
+    pen.end_fill()
+
+
+heart()
---
+++ SC-GS/train_gui.sh
@@ -0,0 +1,1 @@
+python train_gui.py --source_path dataset/jumpingjacks --model_path outputs/jumpingjacks --deform_type node --node_num 512 --is_blender --eval --gui --gt_alpha_mask_as_scene_mask --local_frame --resolution 2 --W 800 --H 800
